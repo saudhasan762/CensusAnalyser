f6020cc4ea07d5642b0bf9a51f51b7f5ee81135c
saudhasan762 <azanali152@gmail.com>
2021-03-10 21:31:58 +0530

    Created test class to match the number of records

--- test/java/censusanalyser/CensusAnalyserTest.java
+++ test/java/censusanalyser/CensusAnalyserTest.java
@@ -4,7 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
